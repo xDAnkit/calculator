071e4525bc8bf1cc2ca2e3aa43537d5954aeb2ef
ashwini1011 <ashwinipote555@gmail.com>
2022-05-22 00:55:49 +0530

    added substraction method and comments

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+/**
+ * @description Method is used to add two Numbers.
+ * @param {Number} a
+ * @param {Number} b
+ * @returns {Number}
+ */
 const add = (a, b) => {
-  const result = a + b;
-  return result;
-};
+  const result = a + b
+  return result
+}
 
-console.log(add(2, 90)); // 92
+/**
+ * @description Method is used to substract two Numbers.
+ * @param {Number} a
+ * @param {Number} b
+ * @returns {Number}
+ */
+const substract = (a, b) => {
+  const result = a - b
+  return result
+}
+
+console.log(add(2, 90)) // 92
+console.log(substract(21, 10))
